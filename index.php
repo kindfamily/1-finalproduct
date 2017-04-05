64ea42ef6cdd653e815503f38a0b1de65dc98e6f
tuntunkimpo <tuntunkimpo@gmail.com>
2017-04-05 23:42:04 +0900

    Create index.php
    grid file

---
+++ index.php
@@ -0,0 +1,56 @@
+<DOCTYPE! html>
+<html>
+    <head>
+       <style>
+           
+           header{
+               border: 1px solid green;
+               height: 28%;
+               
+           }
+           article{
+                border: 1px solid blue;              
+                padding: 2px;
+                height: 60%;
+           }
+           section{
+               padding: 2px;
+               border: 1px solid red;
+               word-break: keep-all;
+               box-sizing: content-box;
+           }
+           footer{
+               border: 1px solid black;
+               
+               height: 10%;
+           }
+           @media screen and (min-width:600px) {
+              section {
+                display: inline-block;
+                width: 24%;
+
+               }
+            }
+
+        </style>
+    </head>
+    <body>
+                
+        <header>
+            
+        </header>
+        <article>
+            
+             <section>
+                 안녕하세요 반갑습니다 오늘은 여러분을 만나서 반갑습니다. 튼튼영어의 미래는 여러분이 책임지고 있습니다 
+
+             </section>
+             <section>section1</section>
+             <section>section1</section>
+             <section>section1</section>
+        </article>
+        <footer>
+            
+        </footer>
+    </body>
+</html>
